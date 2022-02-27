0de7bbcddc741c6c3b17e4646bf34e6608ad433e
Michael Katz <mike.l.katz@gmail.com>
2022-02-27 09:54:49 +0200

    Make tests quits when failing
    This will make jest quit once all operations are concluded.

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -1,20 +1,30 @@
-import { test } from '@jest/globals';
-import Puppeteer from 'puppeteer';
+import Puppeteer, { Browser } from "puppeteer";
 
 //TODO hot module reload for quick feedback cycle
 //TODO proper stack traces for unhandled promise rejections
 
-test('a customer is able to add a product to the shopping cart', async () => {
-  const browser = await Puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://localhost:3000');
+let browser: Browser;
+beforeAll(async () => {
+  browser = await Puppeteer.launch();
+});
 
-  const addToCart = await page.$('aria/Add to cart');
-  expect(addToCart).not.toBeNull();
+afterAll(() => {
+  return browser.close();
+});
 
-  await addToCart!.click();
+test(
+  "a customer is able to add a product to the shopping cart",
+  async () => {
+    const page = await browser.newPage();
+    await page.goto("http://localhost:3000");
 
-  expect(await page.$('aria/1 item in cart')).not.toBeNull();
+    const addToCart = await page.$("aria/Add to cart");
+    expect(addToCart).not.toBeNull();
 
-  await browser.close();
-}, 60 * 1000);
+    await addToCart!.click();
+
+    expect(await page.$("aria/1 item in cart")).not.toBeNull();
+  },
+  60 * 1000
+);
+
