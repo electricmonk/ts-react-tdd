4beec1ac356ae170b1641aac28ca8e711f379afe
Michael Katz <mike.l.katz@gmail.com>
2022-04-12 18:13:50 +0300

    Add UI Design
    This adds some color and layout to our shop, using ChakraUI.
    Issue: https://github.com/electricmonk/ts-react-tdd/issues/8

--- packages/client/src/components/Cart.tsx
+++ packages/client/src/components/Cart.tsx
@@ -1,22 +1,26 @@
-import {CartAdapter} from "../adapters/cart";
+import { CartAdapter } from "../adapters/cart";
 import React from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 interface CartProps {
-    cartAdapter: CartAdapter;
-    cartId: string;
-
+  cartAdapter: CartAdapter;
+  cartId: string;
 }
 
-export const Cart: React.FC<CartProps> = ({cartId, cartAdapter}) => {
-    const navigate = useNavigate();
+export const Cart: React.FC<CartProps> = ({ cartId, cartAdapter }) => {
+  const navigate = useNavigate();
 
-    const checkout = async () => {
-         const orderId = await cartAdapter.checkout(cartId)
-        navigate("/order-summary/" + orderId)
-    }
+  const checkout = async () => {
+    const orderId = await cartAdapter.checkout(cartId);
+    navigate("/order-summary/" + orderId);
+  };
 
-    return <section>
-        <button aria-label="Checkout" role="button" onClick={checkout}>Checkout</button>
+  return (
+    <section>
+      <button aria-label="Checkout" role="button" onClick={checkout}>
+        Checkout
+      </button>
     </section>
-}
+  );
+};
+
--- packages/client/src/components/Shop.tsx
+++ packages/client/src/components/Shop.tsx
@@ -1,47 +1,55 @@
-import React, {useEffect, useState} from "react";
-import {CartAdapter} from "../adapters/cart";
-import { ProductCatalog} from "../adapters/productCatalog";
-import {useNavigate} from "react-router-dom";
-import {Product} from "@ts-react-tdd/server/src/types";
+import React, { useEffect, useState } from "react";
+import { CartAdapter } from "../adapters/cart";
+import { ProductCatalog } from "../adapters/productCatalog";
+import { useNavigate } from "react-router-dom";
+import { Product } from "@ts-react-tdd/server/src/types";
+import { ProductItem } from "./ProductItem";
+import { Button, Grid, GridItem } from "@chakra-ui/react";
 
 interface ShopProps {
-    cartAdapter: CartAdapter;
-    productCatalog: ProductCatalog
-    cartId: string;
+  cartAdapter: CartAdapter;
+  productCatalog: ProductCatalog;
+  cartId: string;
 }
 
-export const Shop: React.FC<ShopProps> = ({cartAdapter, cartId, productCatalog}) => {
-    const [itemCount, setItemCount] = useState<number>(0);
-    const [products, setProducts] = useState<Product[]>([])
-    const addItem = async (productId: Product["id"]) => {
-        await cartAdapter.addItem(cartId, productId);
-        setItemCount(await cartAdapter.getCount(cartId));
-    };
+export const Shop: React.FC<ShopProps> = ({ cartAdapter, cartId, productCatalog }) => {
+  const [itemCount, setItemCount] = useState<number>(0);
+  const [products, setProducts] = useState<Product[]>([]);
+  const addItem = async (productId: Product["id"]) => {
+    await cartAdapter.addItem(cartId, productId);
+    setItemCount(await cartAdapter.getCount(cartId));
+  };
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        cartAdapter.getCount(cartId).then(setItemCount);
-        productCatalog.findAllProducts().then(setProducts)
-    }, []);
+  useEffect(() => {
+    cartAdapter.getCount(cartId).then(setItemCount);
+    productCatalog.findAllProducts().then(setProducts);
+  }, []);
 
-    const viewCart = () => {
-        navigate('/cart');
-    }
+  const viewCart = () => {
+    navigate("/cart");
+  };
 
-    return (
-        <section>
-            <p aria-label={`${itemCount} items in cart`}>{itemCount} items in cart</p>
-            {itemCount && <button aria-label="View cart" role="button" onClick={viewCart}>View cart</button>}
+  return (
+    <section>
+      <p aria-label={`${itemCount} items in cart`}>{itemCount} items in cart</p>
+      {itemCount && (
+        <button aria-label="View cart" role="button" onClick={viewCart}>
+          View cart
+        </button>
+      )}
 
-            {products.map(({title, id}) => <div key={id} aria-label={title}>
-                <h3>{title}</h3>
-                <button onClick={() => addItem(id)} aria-label="Add to cart" role="button">
-                    Add
-                </button>
-            </div>)}
-
-
-        </section>
-    );
+      <Grid h="200px" templateColumns="repeat(5, 1fr)" gap={6}>
+        {products.slice(0, 10).map(({ title, id, image }, index) => (
+          <GridItem key={id} aria-label={title}>
+            <ProductItem title={title} key={id} imageUrl={image!} />
+            <Button onClick={() => addItem(id)} aria-label="Add to cart" role="button">
+              Add
+            </Button>
+          </GridItem>
+        ))}
+      </Grid>
+    </section>
+  );
 };
