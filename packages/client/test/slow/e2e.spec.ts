2a93b640d0be34614c70ea96491bd7fde056331a
Shai Yallin <shai.yallin@gmail.com>
2022-07-04 15:16:19 +0300

    added e2e for cart persist

--- packages/client/test/slow/e2e.spec.ts
+++ packages/client/test/slow/e2e.spec.ts
@@ -44,3 +44,27 @@ test(
     },
     60 * 1000
 );
+
+
+test(
+    "cart is retained between renders", // this test is here because I couldn't make this test fail as a fast-integretive test
+    async () => {
+        await axios.post<void>(`http://localhost:8080/products/`, aProduct());
+        const context = await browser.createIncognitoBrowserContext()
+
+        const page = await context.newPage();
+        await page.goto("http://localhost:3000");
+
+        const addToCart = await page.waitForSelector("aria/Add to cart");
+        expect(addToCart).not.toBeNull();
+        await addToCart!.click();
+
+        expect(await page.waitForSelector("aria/1 items in cart")).not.toBeNull();
+        
+        await page.reload();
+
+        expect(await page.waitForSelector("aria/1 items in cart")).not.toBeNull();
+    
+    },
+    60 * 1000
+);
