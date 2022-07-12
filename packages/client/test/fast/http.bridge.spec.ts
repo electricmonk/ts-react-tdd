c595083dfb730970b2ebe5d5fe3238c5243600d0
Shai Yallin <shai.yallin@gmail.com>
2022-07-12 22:52:15 +0300

    table test

--- packages/client/test/fast/http.bridge.spec.ts
+++ packages/client/test/fast/http.bridge.spec.ts
@@ -1,42 +1,99 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 import Cookies from "js-cookie";
-import { nanoid } from "nanoid";
 import { unwireHttpCalls, wireHttpCallsTo } from "../../src/adapters/express.http.bridge";
 
-describe('the http bridge', () => {
-  afterEach(unwireHttpCalls);
+const cases: Case[] = [
+  get('json', {
+    handler: (req, res) => res.json({foo: "bar"}), 
+    responseMatch: {data: {foo: "bar"}}
+  }),
+  post('echo json', {
+    handler: (req, res) => res.json(req.body), 
+    requestOptions: {data: {foo: "bar"}}, 
+    responseMatch: {data: {foo: "bar"}}
+  }),
+  get('with url param', {
+    path: "/:what", 
+    handler: (req, res) => res.send(req.params.what), 
+    requestOptions: {url: "/foobarbaz"}, 
+    responseMatch: {data: "foobarbaz"}
+  }),
+  get('status defaults to 200', {
+    handler: (req, res) => res.send('foo'), 
+    responseMatch: {status: 200}
+  }),
+  post('sends status code', {
+    handler: (req, res) => res.sendStatus(201), 
+    responseMatch: {status: 201}
+  }),
+  get('echoes headers', {
+    handler: (req, res) => {
+      res
+        .set(Object.fromEntries(Object.entries(req.headers).filter(([key]) => key.startsWith('x-'))))
+        .end()
+    },
+    requestOptions: {headers: {"x-foo": "foo", "x-bar": "baz"}},
+    responseMatch: {headers: expect.objectContaining({"x-foo": "foo", "x-bar": "baz"})},
+  }),
+];
 
-  test('get json', async () => {
-    const obj = {foo: "bar"};
+type Method = 'post' | 'get';
+type Case = {
+  method: Method,
+  description: string,
+  path?: string,
+  handler: (req: express.Request, res: express.Response) => void,
+  requestOptions?: AxiosRequestConfig,
+  responseMatch: Partial<AxiosResponse>,
+  toString: () => string,
+}
 
+function get(description: string, testCase: Omit<Case, "description" | "method">): Case {
+  return {description, method: 'get', ...testCase, toString: () => `get ${description}`}
+}
+
+function post(description: string, testCase: Omit<Case, "description" | "method">): Case {
+  return {description, method: 'post', ...testCase, toString: () => `post ${description}`}
+}
+
+
+afterEach(unwireHttpCalls);
+
+describe.each(['http', 'https'])('%s', (proto) => {
+  const baseURL = `${proto}://localhost`;
+  
+  test.each(cases)('%s', async ({method, path, handler, requestOptions, responseMatch}) => {
+
+    const request = axios.create({baseURL});
+  
     setupRoutes((route) => {
-      route.get('/json', (req, res) => {
-        res.json(obj);
-      })
+      switch (method) {
+        case 'get':
+          route.get(path || '/', handler);
+          break;
+       case 'post':
+          route.post(path || '/', handler);
+          break;
+        default:
+          throw new Error('unsupported method ' + method)
+      }
     });
-
-    await expect(axios.get(`http://localhost/json`)).resolves.toEqual(expect.objectContaining({data: obj}));
+  
+    const options = {
+      url: '/',
+      method,
+      ...requestOptions,
+    }
+    
+    await expect(request(options)).resolves.toEqual(expect.objectContaining(responseMatch));
   })
 
-  test('post json', async () => {
-    setupRoutes((route) => {
-      route.post('/echo/json', (req, res) => {
-        res.json(req.body);
-      })
-    });
-
-    const data = {foo: "bar"};
-
-    await expect(axios.post(`http://localhost/echo/json`, data)).resolves.toEqual(expect.objectContaining({data}));
-
-  })
-
-  // fails because node_modules/jsdom/lib/jsdom/living/helpers/http-request.js thinks response.headers["set-cookie"] is an array
-  test.skip('get and set cookies', async () => {
+  test.skip('get and set cookies', async () => {  // fails because node_modules/jsdom/lib/jsdom/living/helpers/http-request.js thinks response.headers["set-cookie"] is an array
     const cookieName = 'fookie';
 
     setupRoutes((route) => {
@@ -50,23 +107,10 @@ describe('the http bridge', () => {
     });
 
     Cookies.set(cookieName, 'bar');
-    await expect(axios.get(`http://localhost/cookie`)).resolves.toEqual(expect.objectContaining({headers: expect.objectContaining({"set-cookie": `${cookieName}=baz`})}));
+    await expect(axios.get(`${proto}://localhost/cookie`)).resolves.toEqual(expect.objectContaining({headers: expect.objectContaining({"set-cookie": `${cookieName}=baz`})}));
 
   })
 
-
-  test('get with string url param', async () => {
-    setupRoutes((route) => {
-      route.get('/echo/param/:what', (req, res) => {
-        const { what } = req.params;
-
-        res.send(what);
-      })
-    });
-
-    const word = nanoid();
-    await expect(axios.get(`http://localhost/echo/param/${word}`)).resolves.toEqual(expect.objectContaining({data: word}));
-  })
 
   test('keeps state between requests', async () => {
     setupRoutes((route) => {
@@ -76,70 +120,21 @@ describe('the http bridge', () => {
         res.json(count);
       })
     });
-    await expect(axios.get(`http://localhost/add`)).resolves.toEqual(expect.objectContaining({data: 1}));
-    await expect(axios.get(`http://localhost/add`)).resolves.toEqual(expect.objectContaining({data: 2}));
-    await expect(axios.get(`http://localhost/add`)).resolves.toEqual(expect.objectContaining({data: 3}));
+    await expect(axios.get(`${proto}://localhost/add`)).resolves.toEqual(expect.objectContaining({data: 1}));
+    await expect(axios.get(`${proto}://localhost/add`)).resolves.toEqual(expect.objectContaining({data: 2}));
+    await expect(axios.get(`${proto}://localhost/add`)).resolves.toEqual(expect.objectContaining({data: 3}));
   });
 
-  test('status defaults to 200', async () => {
-    setupRoutes(route => {
-      route.get('/status', (req, res) => {
-        res.send('foo')
-      })
-    })
-
-    await expect(axios.get(`http://localhost/status`)).resolves.toEqual(expect.objectContaining({status: 200}));
-
-  })
-
-
-  test('post sends status code', async () => {
-    setupRoutes(route => {
-      route.post('/status', (req, res) => {
-        const {status} = req.body;
-        res.sendStatus(parseInt(status));
-      })
-    })
-
-    const status = 201;
-    await expect(axios.post(`http://localhost/status`, {status})).resolves.toEqual(expect.objectContaining({status}));
-
-  })
-
-  test('get sends status code', async () => {
-    setupRoutes(route => {
-      route.get('/status/:status', (req, res) => {
-        const {status} = req.params;
-        res.sendStatus(parseInt(status));
-      })
-    })
-
-    const status = 203;
-    await expect(axios.get(`http://localhost/status/${status}`)).resolves.toEqual(expect.objectContaining({status}));
-
-  })
-
-
-  test('echoes headers', async () => {
-    setupRoutes(route => {
-      route.get('/echo/headers', (req, res) => {
-        res
-          .set(Object.fromEntries(Object.entries(req.headers).filter(([key]) => key.startsWith('x-'))))
-          .end();
-      })
-    })
-
-    const headers =  {"x-foo": "foo", "x-bar": "baz"};
-    expect(axios.get(`http://localhost/echo/headers`, {headers})).resolves.toEqual(expect.objectContaining({headers: expect.objectContaining(headers)}));
-  })
 
 })
+
 
 
 function setupRoutes(makeRoutes: (router: express.Router) => any) {
   const app = express();
   const router = express.Router();
   makeRoutes(router);
+  app.use(cors());
   app.use(cookieParser())
   app.use(bodyParser.json());
   app.use(router);
