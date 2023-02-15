eb9ea0eb097f75f9bcf9cc7f1629ab2775dea8c9
Shai Yallin <shai.yallin@gmail.com>
2023-02-15 17:27:15 +0200

    use vite

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitest/config'
+import { createHtmlPlugin } from 'vite-plugin-html';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    build: {
+        outDir: 'dist',
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        // setupFiles: ["./setupTests.ts"],
+        include: ['./test/*.spec.ts', './test/*.spec.tsx'],
+    },
+    plugins: [
+        createHtmlPlugin({
+            entry: 'src/index.tsx',
+            template: "./index.html",
+        }),
+        react({
+            include: '**/*.tsx',
+        }),
+    ],
+    server: {
+        port: 3000,
+        host: '0.0.0.0'
+    }
+});
